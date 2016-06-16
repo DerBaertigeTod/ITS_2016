a711c33cf1c15cb2319cd7b43c7ac2a09894cb94
Exellent1988 <axellodyga@googlemail.com>
2016-06-16 16:17:46 +0200

    vector calc for color

--- Scripts/python/color.py
+++ Scripts/python/color.py
@@ -1,11 +1,10 @@
-def get_color_name(r,g,b):
+def get_color_name(r,g,colors[c_val]):
 	deltas = [0,0,0,0]
 	i = 0
-	colors ={'Red':[255,0,0],'orange':[255,165,0],'yellow':[255,255,0],'green':[0,128,0],'blue':[0,0,255],'violett':[238,130,238],'brown':[165,42,42]}
+	colors ={'Red':[255,0,0],'orange':[255,165,0],'yellow':[255,255,0],'green':[0,128,0],'colors[c_val]lue':[0,0,255],'violett':[238,130,238],'colors[c_val]rown':[165,42,42]}
 	for c_key, c_val in enumerate(colors):
-		 	deltas[i] = c_val[0] - r
-		 	deltas[i] += c_val[1] - g
-		 	deltas[i] += c_val[2] - b
+		 	deltas[i] = sqrt((colors[c_val][0]-r)*(colors[c_val][0]-r)+(colors[c_val][1]-g)*(colors[c_val][1]-g)+(colors[c_val][2]-b)*(colors[c_val][2]-b))
+		 	
 			i++
 	winner = min(deltas, key=deltas.get)
 
@@ -14,3 +13,21 @@ def get_color_name(r,g,b):
 
 get_color_name()	 	
 		 	 
+
+def _get_color(rgcolors[c_val])
+print "Punkt1 x y z"
+x0 = float(raw_input())
+y0 = float(raw_input())
+z0 = float(raw_input())
+print "Punkt2 x y z"
+x1 = float(raw_input())
+y1 = float(raw_input())
+z1 = float(raw_input())
+#prfloat "[", x0, y0, z0, "] ", " [",x1, y1,z1, "]"
+
+a = [x0, y0, z0]
+colors[c_val] = [x1, y1, z1]
+
+acolors[c_val]stand = sqrt((colors[c_val][0]-r)*(colors[c_val][0]-r)+(colors[c_val][1]-g)*(colors[c_val][1]-g)+(colors[c_val][2]-b)*(colors[c_val][2]-b))
+
+print "acolors[c_val]stand", acolors[c_val]stand
